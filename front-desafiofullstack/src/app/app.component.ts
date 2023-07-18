2558d061811be1a70270603aab182edf2e0d2a29
Leonardo Pereira <leonardocoutope@gmail.com>
2023-07-18 15:42:21 -0300

    Criando estrutura inicial do front e back.

---
+++ front-desafiofullstack/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dff-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'dff';
+}
